0e9a76915aed12d414efff442643c54bd71b6c3d
David <45101011+DavidJerman@users.noreply.github.com>
2022-01-12 15:45:19 +0100

    Implemented LinkedList.h and DLinkedList.h partially. Watched the rest of the videos.

---
+++ linkedLists/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "LinkedList.h"
+
+int main() {
+
+    LinkedList linkedList;
+    linkedList.push_front(10);
+    linkedList.pop_front();
+    linkedList.pop_front();
+    linkedList.push_back(12);
+    linkedList.push_front(11);
+    std::cout << "Front: " << linkedList.front() << std::endl;
+    std::cout << "Back: " << linkedList.back() << std::endl;
+    linkedList.insert(42, 1);
+    linkedList.insert(43, 3);
+    linkedList.insert(41, 0);
+    std::cout << "Front: " << linkedList.front() << std::endl;
+    linkedList.erase(1);
+    linkedList.erase(3);
+    linkedList.erase(0);
+    std::cout << "Front: " << linkedList.front() << std::endl;
+    std::cout << "At 0: " << linkedList.at(0) << std::endl;
+    std::cout << "At 1: " << linkedList.at(1) << std::endl;
+    std::cout << "Size: " << linkedList.size() << std::endl;
+    std::cout << "From back 0: " << linkedList.value_n_from_back(0) << std::endl;
+    std::cout << "From back 1: " << linkedList.value_n_from_back(1) << std::endl;
+    linkedList.push_front(10);
+    linkedList.push_back(9);
+    linkedList.reverse();
+    linkedList.remove_value(42);
+
+    return 0;
+}
